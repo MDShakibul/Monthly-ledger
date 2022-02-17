5e03d98c3542c917d6004ea842eb94478f40f9da
MDShakibul <sakib6610@gmail.com.com>
2022-02-17 22:06:38 +0600

    done

--- app.js
+++ app.js
@@ -1,55 +1,94 @@
 /* expenses function */
-function allCost(expenseFood, expenseRent, expenseClothes){
-    let foodCost =parseFloat (document.getElementById(expenseFood).value);
-    let rentCost = parseFloat (document.getElementById(expenseRent).value);
-    let clothesCost =parseFloat (document.getElementById(expenseClothes).value);
+function allCost(expenseFood, expenseRent, expenseClothes) {
+  let foodCost = parseFloat(document.getElementById(expenseFood).value);
+  let rentCost = parseFloat(document.getElementById(expenseRent).value);
+  let clothesCost = parseFloat(document.getElementById(expenseClothes).value);
 
-    let totaCost = foodCost+rentCost+clothesCost;
-
+  if ((foodCost >= 0 || isNaN(foodCost)) && (rentCost >= 0 || isNaN(rentCost)) && (clothesCost >= 0 || isNaN(clothesCost))) {
+    let totaCost = foodCost + rentCost + clothesCost;
     return totaCost;
-
+  } else {
+    return -1;
+  }
 }
 
-function getValue(getId){
-    return parseFloat (document.getElementById(getId).value);
+function getValue(getId) {
+  return parseFloat(document.getElementById(getId).value);
 }
 
-
-document.getElementById('btn-calculator').addEventListener('click',function(){
-
-
-    let totalExpenses = allCost('food','rent','clothes');
-    let income = getValue('total-income');
-    if(!isNaN(totalExpenses) && !isNaN(income) ){
-        if(income >= totalExpenses){
-            let balance = income - totalExpenses;
-            document.getElementById('total-expenses').innerHTML = totalExpenses;
-            document.getElementById('total-balance').innerHTML = balance;
-        }else{
-            document.getElementById('more_expense_error').style.display = "block";
-            document.getElementById('input_error').style.display = "none";
+document
+  .getElementById("btn-calculator")
+  .addEventListener("click", function () {
+    let totalExpenses = allCost("food", "rent", "clothes");
+    let income = getValue("total-income");
+    /* income validation */
+    if (!isNaN(totalExpenses) && !isNaN(income)) {
+      if (totalExpenses != -1 && income > 0) {
+        if (income >= totalExpenses) {
+          let balance = income - totalExpenses;
+          document.getElementById("total-expenses").innerHTML = totalExpenses;
+          document.getElementById("total-balance").innerHTML = balance;
+          document.getElementById("more_expense_error").style.display = "none";
+          document.getElementById("negative_error").style.display = "none";
+          document.getElementById("input_error").style.display = "none";
+        } else {
+          document.getElementById("more_expense_error").style.display = "block";
+          document.getElementById("input_error").style.display = "none";
+          document.getElementById("negative_error").style.display = "none";
         }
-    }else{
-        document.getElementById('more_expense_error').style.display = "none";
-        document.getElementById('input_error').style.display = "block";
+      } else {
+        document.getElementById("more_expense_error").style.display = "none";
+        document.getElementById("negative_error").style.display = "block";
+        document.getElementById("input_error").style.display = "none";
+      }
+    } else {
+      document.getElementById("more_expense_error").style.display = "none";
+      document.getElementById("negative_error").style.display = "none";
+      document.getElementById("input_error").style.display = "block";
     }
+  });
+ 
 
-    
+/*   saving section */
+document.getElementById("btn-saving").addEventListener("click", function () {
+  let income = getValue("total-income");
+  let percentage = getValue("percentage");
+       
+  let totalExpenses = allCost("food", "rent", "clothes");
 
-})
 
-document.getElementById('btn-saving').addEventListener('click', function(){
-    let income = getValue('total-income');
-    let percentage = getValue('percentage');
+/* saving validation */
+  if (!isNaN(percentage)) {
+    if (percentage >= 0) {
+      let savingAmount = (income * percentage) / 100;
 
-    let savingAmount  = (income*percentage)/100;
 
-    document.getElementById('saving-amount').innerHTML = savingAmount;
-    let totalExpenses = allCost('food','rent','clothes');
-    let balance = income - totalExpenses;
+      let balance = income - totalExpenses;
 
-    let remainingBalance = balance - savingAmount;
+      let remainingBalance = balance - savingAmount;
 
-    document.getElementById('remain-balance').innerHTML = remainingBalance;
+      if (remainingBalance >= 0) {
+        document.getElementById("saving-amount").innerHTML = savingAmount;
+        document.getElementById("remain-balance").innerHTML = remainingBalance;
 
-})
+        document.getElementById("more_saving_error").style.display = "none";
+        document.getElementById("percentage_negative_error").style.display = "none";
+        document.getElementById("percentage_input_error").style.display = "none";
+      } else {
+        document.getElementById("more_saving_error").style.display = "block";
+        document.getElementById("percentage_negative_error").style.display = "none";
+        document.getElementById("percentage_input_error").style.display = "none";
+      }
+    } else {
+      document.getElementById("more_saving_error").style.display = "none";
+      document.getElementById("percentage_negative_error").style.display = "block";
+      document.getElementById("percentage_input_error").style.display = "none";
+    }
+  } else {
+    document.getElementById("more_saving_error").style.display = "none";
+    document.getElementById("percentage_negative_error").style.display = "none";
+    document.getElementById("percentage_input_error").style.display = "block";
+  }
+
+
+});
