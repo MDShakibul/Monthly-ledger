dc6dba3c76e897c38d58af80fb55fe4d81d61f26
MDShakibul <sakib6610@gmail.com.com>
2022-02-17 19:32:14 +0600

    validation done

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
-document.getElementById('btn-calculator').addEventListener('click',function(){
-    let foodCost =parseFloat (document.getElementById('food-cost').value);
-    let rentCost = parseFloat (document.getElementById('rent-cost').value);
-    let clothesCost =parseFloat (document.getElementById('clothes-cost').value);
+/* expenses function */
+function allCost(expenseFood, expenseRent, expenseClothes){
+    let foodCost =parseFloat (document.getElementById(expenseFood).value);
+    let rentCost = parseFloat (document.getElementById(expenseRent).value);
+    let clothesCost =parseFloat (document.getElementById(expenseClothes).value);
 
     let totaCost = foodCost+rentCost+clothesCost;
-    
-    let income = parseFloat (document.getElementById('total-income').value);
 
-    let balance = income - totaCost;
-    document.getElementById('total-expenses').innerHTML = totaCost;
-    document.getElementById('total-balance').innerHTML = balance;
+    return totaCost;
+
+}
+
+function getValue(getId){
+    return parseFloat (document.getElementById(getId).value);
+}
+
+
+document.getElementById('btn-calculator').addEventListener('click',function(){
+
+
+    let totalExpenses = allCost('food','rent','clothes');
+    let income = getValue('total-income');
+    if(!isNaN(totalExpenses) && !isNaN(income) ){
+        if(income >= totalExpenses){
+            let balance = income - totalExpenses;
+            document.getElementById('total-expenses').innerHTML = totalExpenses;
+            document.getElementById('total-balance').innerHTML = balance;
+        }else{
+            document.getElementById('more_expense_error').style.display = "block";
+            document.getElementById('input_error').style.display = "none";
+        }
+    }else{
+        document.getElementById('more_expense_error').style.display = "none";
+        document.getElementById('input_error').style.display = "block";
+    }
+
+    
 
 })
 
 document.getElementById('btn-saving').addEventListener('click', function(){
-    let income = parseFloat (document.getElementById('total-income').value);
-    let percentage = parseFloat (document.getElementById('percentage').value);
+    let income = getValue('total-income');
+    let percentage = getValue('percentage');
 
     let savingAmount  = (income*percentage)/100;
 
     document.getElementById('saving-amount').innerHTML = savingAmount;
-
-    let foodCost =parseFloat (document.getElementById('food-cost').value);
-    let rentCost = parseFloat (document.getElementById('rent-cost').value);
-    let clothesCost =parseFloat (document.getElementById('clothes-cost').value);
-    let totaCost = foodCost+rentCost+clothesCost;
-    let balance = income - totaCost;
+    let totalExpenses = allCost('food','rent','clothes');
+    let balance = income - totalExpenses;
 
     let remainingBalance = balance - savingAmount;
 
